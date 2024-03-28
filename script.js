4784904e32269a981be8dcf8ee819684dbbdd2ed
yashrajbharticybtekk <y.bharti@cybtekk.com>
2024-03-29 01:52:12 +0530

    new seasonal leaders rocket data

--- script.js
+++ script.js
@@ -39,6 +39,128 @@ const pokemonTypes = [
   "dragon",
   "normal",
 ];
+const typeChart = {
+  bug: { fighting: 0.5, grass: 0.5, ground: 0.5, fire: 2, flying: 2, rock: 2 },
+  dark: { dark: 0.5, ghost: 0.5, psychic: 0.5, bug: 2, fairy: 2, fighting: 2 },
+  dragon: {
+    electric: 0.5,
+    fire: 0.5,
+    grass: 0.5,
+    water: 0.5,
+    dragon: 2,
+    fairy: 2,
+    ice: 2,
+  },
+  electric: { electric: 0.5, flying: 0.5, steel: 0.5, ground: 2 },
+  fairy: {
+    bug: 0.5,
+    dark: 0.5,
+    dragon: 0.5,
+    fighting: 0.5,
+    poison: 2,
+    steel: 2,
+  },
+  fighting: { bug: 0.5, dark: 0.5, rock: 0.5, fairy: 2, flying: 2, psychic: 2 },
+  fire: {
+    bug: 0.5,
+    fire: 0.5,
+    grass: 0.5,
+    ice: 0.5,
+    steel: 0.5,
+    ground: 2,
+    rock: 2,
+    water: 2,
+  },
+  flying: {
+    bug: 0.5,
+    fighting: 0.5,
+    grass: 0.5,
+    ground: 0.5,
+    electric: 2,
+    ice: 2,
+    rock: 2,
+  },
+  ghost: {
+    bug: 0.5,
+    fighting: 0.5,
+    normal: 0.5,
+    poison: 0.5,
+    dark: 2,
+    ghost: 2,
+  },
+  grass: {
+    electric: 0.5,
+    grass: 0.5,
+    ground: 0.5,
+    water: 0.5,
+    bug: 2,
+    fire: 2,
+    flying: 2,
+    ice: 2,
+    poison: 2,
+  },
+  ground: { electric: 0.5, poison: 0.5, rock: 0.5, grass: 2, ice: 2, water: 2 },
+  ice: { ice: 0.5, fighting: 2, fire: 2, rock: 2, steel: 2 },
+  normal: { ghost: 0.5, fighting: 2 },
+  poison: {
+    fairy: 0.5,
+    fighting: 0.5,
+    grass: 0.5,
+    poison: 0.5,
+    bug: 0.5,
+    ground: 2,
+    psychic: 2,
+  },
+  psychic: { fighting: 0.5, psychic: 0.5, bug: 2, dark: 2, ghost: 2 },
+  rock: {
+    fire: 0.5,
+    flying: 0.5,
+    normal: 0.5,
+    poison: 0.5,
+    fighting: 2,
+    grass: 2,
+    ground: 2,
+    steel: 2,
+    water: 2,
+  },
+  steel: {
+    bug: 0.5,
+    dragon: 0.5,
+    fairy: 0.5,
+    flying: 0.5,
+    grass: 0.5,
+    ice: 0.5,
+    poison: 0.5,
+    psychic: 0.5,
+    rock: 0.5,
+    steel: 0.5,
+    fighting: 2,
+    fire: 2,
+    ground: 2,
+  },
+  water: { fire: 0.5, ice: 0.5, steel: 0.5, water: 0.5, electric: 2, grass: 2 },
+};
+
+const getWeaknessTypes = (types) => {
+  const weaknesses = {};
+  for (const type of types) {
+    if (type === undefined) continue;
+
+    const typeMultipliers = typeChart[type.toLowerCase()] || {};
+    for (const otherType in typeMultipliers) {
+      if (!weaknesses[otherType]) weaknesses[otherType] = 1;
+
+      weaknesses[otherType] *= typeMultipliers[otherType];
+    }
+  }
+
+  const sortedWeaknesses = Object.entries(weaknesses)
+    .sort(([, a], [, b]) => b - a)
+    .filter(([, value]) => value > 1)
+    .map(([type]) => type);
+  console.log(sortedWeaknesses);
+  return sortedWeaknesses;
+};
 
 const getRocketLeaderData = async (leader, format = ".txt") => {
   // Replace ./data.json with your JSON feed
@@ -114,6 +236,7 @@ const fragmentTypesForSingleGroup = (group, pokedex, typeWrappers) => {
             .replace("-origin", "")
             .replace("-mega", "")
             .replace("-standard", "")
+            .replace("-sky", "")
             .replace("-galarian", "") === dex.name.english.toLowerCase()
         )
           return dex;
@@ -126,6 +249,7 @@ const fragmentTypesForSingleGroup = (group, pokedex, typeWrappers) => {
           .replace("-origin", "")
           .replace("-mega", "")
           .replace("-standard", "")
+          .replace("-sky", "")
           .replace("-galarian", "")
       );
     }
@@ -135,13 +259,11 @@ const fragmentTypesForSingleGroup = (group, pokedex, typeWrappers) => {
 let commonTypes = [];
 const buildUI = (typeWrapper, types, index) => {
   let refinedTypes = new Set();
-  for (const type of types) {
-    if (index === 0) {
-      for (const weakness of weaknessMap.get(type)) {
-        refinedTypes.add(weakness);
-      }
-    } else {
-      refinedTypes.add(type);
+  if (index === 0) {
+    refinedTypes = getWeaknessTypes(types);
+  } else {
+    for (const type of types) {
+      refinedTypes.add(type.toLowerCase());
     }
   }
   refinedTypes = [...refinedTypes];
